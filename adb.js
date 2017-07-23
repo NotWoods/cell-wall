6f1122255f55f356c3626e54096b6c2971488cb6
Tiger Oakes <tigeroakes@gmail.com>
2017-07-23 12:02:38 -0700

    Shifting some scripts into utils folder

--- adb.js
+++ adb.js
@@ -1,41 +1,6 @@
 'use strict';
-const adbkit = require('adbkit');
 const chalk = require('chalk');
-const secret = require('./secret.json');
-
-const adb = adbkit.createClient();
-
-/**
- * Returns a list of device paths from ADB. (looks like `usb:xxx`)
- * Since one of my phones doesn't have a serial number for some reason,
- * this allows the devices to be identified anyways.
- * @returns {Promise<string[]>}
- */
-async function getCellWallDevices() {
-	return (await adb.listDevicesWithPaths())
-		.filter(device => device.type === 'device')
-		.map(device => device.path);
-}
-
-/**
- * Runs a shell command on every device
- * @param {string} command
- * @param {string[]|Promise<string[]>} [devices] - list of devices
- */
-async function allShell(command, devices = getCellWallDevices()) {
-	const deviceList = await devices;
-	await Promise.all(deviceList.map(id => adb.shell(id, command)));
-}
-
-/**
- * Triggers a button on every connected android device, or only on specified
- * devices.
- * @param {number} keycode - number of the key to use
- * @param {string[]|Promise<string[]>} [devices] - list of devices
- */
-async function triggerButton(keycode, devices) {
-	return allShell(`input keyevent ${keycode}`);
-}
+const { triggerButton, multiShell } = require('./utils/adb');
 
 /**
  * Triggers the power button on every android device
@@ -50,22 +15,27 @@ async function powerAll() {
  * @param {string[]|Promise<string[]>} [devices] - list of devices
  */
 function dockAll(devices) {
-	return allShell(`am broadcast -a android.intent.action.DOCK_EVENT --ei android.intent.extra.DOCK_STATE 1`);
+	return multiShell(
+		'am broadcast -a android.intent.action.DOCK_EVENT' +
+		' --ei android.intent.extra.DOCK_STATE 1',
+		devices
+	);
 }
 /**
  * Docks every phone
  * @param {string[]|Promise<string[]>} [devices] - list of devices
  */
 function undockAll(devices) {
-	return allShell(`am broadcast -a android.intent.action.DOCK_EVENT --ei android.intent.extra.DOCK_STATE 0`);
+	return multiShell(
+		'am broadcast -a android.intent.action.DOCK_EVENT' +
+		' --ei android.intent.extra.DOCK_STATE 0',
+		devices
+	);
 }
 
 
 module.exports = {
 	adb,
-	getCellWallDevices,
-	allShell,
-	triggerButton,
 	powerAll,
 	dockAll,
 	undockAll,
---
+++ utils/adb.js
@@ -0,0 +1,45 @@
+'use strict';
+const adbkit = require('adbkit');
+const chalk = require('chalk');
+
+/**
+ * Returns a list of device paths from ADB. (looks like `usb:xxx`)
+ * Since one of my phones doesn't have a serial number for some reason,
+ * this allows the devices to be identified anyways.
+ * @param {ADBKit} [client] adbkit client
+ * @returns {Promise<string[]>}
+ */
+async function getCellWallDevices(client = adbkit.createClient()) {
+	return (await client.listDevicesWithPaths())
+		.filter(device => device.type === 'device')
+		.map(device => device.path);
+}
+
+/**
+ * Runs a shell command on every device
+ * @param {string} command
+ * @param {string[]} [devices] - list of devices
+ * @param {ADBKit} [client] adbkit client
+ */
+async function multiShell(command, devices, client = adbkit.createClient()) {
+	const deviceList = devices || getCellWallDevices(client);
+	await Promise.all(deviceList.map(id => client.shell(id, command)));
+}
+
+/**
+ * Triggers a button on every connected android device, or only on specified
+ * devices.
+ * @param {number} keycode - number of the key to use
+ * @param {string[]} [devices] - list of devices
+ * @param {ADBKit} [client] adbkit client
+ */
+async function triggerButton(keycode, devices, client) {
+	return multiShell(`input keyevent ${keycode}`, devices, client);
+}
+
+
+module.exports = {
+	getCellWallDevices,
+	multiShell,
+	triggerButton,
+};
