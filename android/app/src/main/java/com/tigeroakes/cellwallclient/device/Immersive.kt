8a6d1c7593051248686d1ce96848f07f3ab4bd21
Tiger Oakes <contact@tigeroakes.com>
2020-11-04 20:42:07 -0800

    Android R immersive mode

--- android/app/src/main/java/com/tigeroakes/cellwallclient/device/Immersive.kt
+++ android/app/src/main/java/com/tigeroakes/cellwallclient/device/Immersive.kt
@@ -1,32 +1,34 @@
 package com.tigeroakes.cellwallclient.device
 
-import android.os.Build.VERSION_CODES
 import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES
 import android.view.View.*
 import android.view.Window
+import android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 object Immersive {
 
-  private val VISIBILITY_FLAGS: Int
-
-  init {
-    var visibilityFlags = SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-      SYSTEM_UI_FLAG_LAYOUT_STABLE or
-      SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-      SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-      SYSTEM_UI_FLAG_FULLSCREEN
-    if (SDK_INT >= VERSION_CODES.KITKAT) {
-      visibilityFlags = visibilityFlags or SYSTEM_UI_FLAG_IMMERSIVE
-    }
-
-    VISIBILITY_FLAGS = visibilityFlags
-  }
+  @Suppress("Deprecation")
+  private const val VISIBILITY_FLAGS = SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+    SYSTEM_UI_FLAG_FULLSCREEN or
+    SYSTEM_UI_FLAG_IMMERSIVE
 
   /**
    * Hides system bars.
    * https://developer.android.com/training/system-ui/immersive
    */
   fun enterImmersiveMode(window: Window) {
-    window.decorView.systemUiVisibility = VISIBILITY_FLAGS
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    if (SDK_INT >= VERSION_CODES.R) {
+      val controller = window.decorView.windowInsetsController!!
+      controller.systemBarsBehavior = BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+      controller.hide(WindowInsetsCompat.Type.systemBars())
+    } else {
+      @Suppress("Deprecation")
+      window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or VISIBILITY_FLAGS
+    }
   }
 }
