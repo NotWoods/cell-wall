6fc42269b8a3ab959bfb60dd97a3025e32d0846d
Tiger Oakes <contact@tigeroakes.com>
2020-11-04 20:23:27 -0800

    Delete old web folder

--- web/server/__mocks__/fs.ts
+++
@@ -1,7 +0,0 @@
-export const readFile = jest.fn().mockResolvedValue('');
-export const writeFile = jest.fn();
-export const access = jest.fn();
-export const ensureFile = jest.fn();
-export const readJson = jest.fn().mockResolvedValue({});
-export const writeJson = jest.fn();
-export const watchJson = jest.fn();
