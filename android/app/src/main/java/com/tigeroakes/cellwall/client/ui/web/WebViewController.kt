e2874a70497175812acc2724500b19ce1d720866
Tiger Oakes <contact@tigeroakes.com>
2020-11-13 21:41:21 -0800

    Disable config file, causes KiKat crashes

--- android/app/src/main/java/com/tigeroakes/cellwall/client/ui/web/WebViewController.kt
+++ android/app/src/main/java/com/tigeroakes/cellwall/client/ui/web/WebViewController.kt
@@ -2,6 +2,7 @@ package com.tigeroakes.cellwall.client.ui.web
 
 import android.webkit.WebView
 import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoRuntimeSettings
 import org.mozilla.geckoview.GeckoSession
 import org.mozilla.geckoview.GeckoView
 
@@ -10,7 +11,10 @@ class WebViewController(webView: GeckoView) {
   private val session = GeckoSession()
 
   init {
-    val runtime = GeckoRuntime.create(webView.context)
+    val settings = GeckoRuntimeSettings.Builder()
+      .configFilePath("")
+      .build()
+    val runtime = GeckoRuntime.create(webView.context, settings)
 
     session.open(runtime)
     webView.setSession(session)
