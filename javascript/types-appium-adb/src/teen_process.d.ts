f15e5ecf4fa700a6a17c48cb6088411c202efaab
Tiger Oakes <contact@tigeroakes.com>
2020-11-24 20:34:04 -0800

    Run format

--- javascript/types-appium-adb/src/teen_process.d.ts
+++ javascript/types-appium-adb/src/teen_process.d.ts
@@ -2,65 +2,65 @@ import { EventEmitter } from 'events';
 import { ChildProcess } from 'child_process';
 
 export interface TeenProcessExecOptions {
-    cwd?: string;
-    env?: { [envVar: string]: string };
-    timeout?: number;
-    killSignal?: string;
-    encoding?: string;
-    ignoreOutput?: boolean;
-    stdio?: string;
-    isBuffer?: boolean;
-    shell?: boolean;
+  cwd?: string;
+  env?: { [envVar: string]: string };
+  timeout?: number;
+  killSignal?: string;
+  encoding?: string;
+  ignoreOutput?: boolean;
+  stdio?: string;
+  isBuffer?: boolean;
+  shell?: boolean;
 }
 
 export interface ShellExecOptions extends TeenProcessExecOptions {
-    /** @default [falsy] Whether to run the given command as root. */
-    privileged?: boolean;
-    /** @default [falsy] Whether to keep root mode after command execution is completed. */
-    keepPrivileged?: boolean;
+  /** @default [falsy] Whether to run the given command as root. */
+  privileged?: boolean;
+  /** @default [falsy] Whether to keep root mode after command execution is completed. */
+  keepPrivileged?: boolean;
 }
 
 export class SubProcess extends EventEmitter {
-    cmd: string;
-    args: ReadonlyArray<string>;
-    proc: ChildProcess | null;
-    opts: { detached?: boolean; encoding?: string };
-    expectingExit: boolean;
-    rep: string;
+  cmd: string;
+  args: ReadonlyArray<string>;
+  proc: ChildProcess | null;
+  opts: { detached?: boolean; encoding?: string };
+  expectingExit: boolean;
+  rep: string;
 
-    constructor(
-        cmd: string,
-        args?: ReadonlyArray<string>,
-        opts?: { detached?: boolean; encoding?: string },
-    );
+  constructor(
+    cmd: string,
+    args?: ReadonlyArray<string>,
+    opts?: { detached?: boolean; encoding?: string },
+  );
 
-    isRunning: boolean;
+  isRunning: boolean;
 
-    emitLines(stream: string, lines: ReadonlyArray<string>): void;
+  emitLines(stream: string, lines: ReadonlyArray<string>): void;
 
-    // spawn the subprocess and return control whenever we deem that it has fully
-    // "started"
-    start(detach?: boolean): Promise<any>;
-    start(
-        startDetector?: ((stdout: any, stderr: any) => boolean) | number | null,
-        detach?: boolean,
-    ): Promise<any>;
-    start(
-        startDetector?: ((stdout: any, stderr: any) => boolean) | number | null,
-        timeoutMs?: number | null,
-        detach?: boolean,
-    ): Promise<void>;
+  // spawn the subprocess and return control whenever we deem that it has fully
+  // "started"
+  start(detach?: boolean): Promise<any>;
+  start(
+    startDetector?: ((stdout: any, stderr: any) => boolean) | number | null,
+    detach?: boolean,
+  ): Promise<any>;
+  start(
+    startDetector?: ((stdout: any, stderr: any) => boolean) | number | null,
+    timeoutMs?: number | null,
+    detach?: boolean,
+  ): Promise<void>;
 
-    handleLastLines(): void;
+  handleLastLines(): void;
 
-    stop(signal?: string, timeout?: number): Promise<number>;
+  stop(signal?: string, timeout?: number): Promise<number>;
 
-    join(allowedExitCodes?: ReadonlyArray<number>): Promise<number>;
+  join(allowedExitCodes?: ReadonlyArray<number>): Promise<number>;
 
-    /*
-     * This will only work if the process is created with the `detached` option
-     */
-    detachProcess(): void;
+  /*
+   * This will only work if the process is created with the `detached` option
+   */
+  detachProcess(): void;
 
-    pid: number;
+  pid: number;
 }
