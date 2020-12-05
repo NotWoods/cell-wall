970a78444f9ece847358290e891433c00f505857
Tiger Oakes <contact@tigeroakes.com>
2020-12-05 10:31:05 -0800

    Update tea list

--- javascript/server/src/decorate/cell-bridge.ts
+++ javascript/server/src/decorate/cell-bridge.ts
@@ -3,6 +3,20 @@ import { toUri, CellManager, CellData } from '@cell-wall/cells';
 
 export const PACKAGE_NAME = 'com.tigeroakes.cellwall.client';
 
+String.prototype.replaceAll = function replaceAll(
+  this: string,
+  str: string | RegExp,
+  newStr: any,
+) {
+  // If a regex pattern
+  if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+    return this.replace(str, newStr);
+  }
+
+  // If a string
+  return this.replace(new RegExp(str, 'g'), newStr);
+};
+
 export function cellBridge(deviceManager: DeviceManager, cells: CellManager) {
   cells.on('state', async (data: CellData) => {
     const device = deviceManager.devices.get(data.serial);
@@ -13,7 +27,9 @@ export function cellBridge(deviceManager: DeviceManager, cells: CellManager) {
 
     await startIntent(device, {
       action: `${PACKAGE_NAME}.DISPLAY`,
-      dataUri: toUri(data.state, base).replace(/&/g, '\\&'),
+      dataUri: toUri(data.state, base)
+        .replaceAll(`&`, '\\&')
+        .replaceAll(`&`, '%27'),
     });
   });
 }
