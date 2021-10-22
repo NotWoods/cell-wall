cf3c68c45d1dd3968f36f6e4aeaa235a34a5e1a1
Tiger Oakes <contact@tigeroakes.com>
2021-10-21 23:39:39 -0700

    deploy

--- client/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { NextHandleFunction, SimpleHandleFunction } from 'connect';
-
-export const assetsMiddleware: NextHandleFunction;
-export const kitMiddleware: SimpleHandleFunction;
-export const prerenderedMiddleware: NextHandleFunction;
