e2f6b5e18386dcee0fcb637a444503ec2d84da01
Tiger Oakes <contact@tigeroakes.com>
2022-03-19 14:49:44 -0700

    Add safari tags

--- javascript/client/src/routes/cell/frame/__layout.svelte
+++ javascript/client/src/routes/cell/frame/__layout.svelte
@@ -60,6 +60,10 @@
 
 <svelte:head>
 	<title>Cell</title>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<meta name="apple-mobile-web-app-title" content="CellWall" />
+	<link rel="apple-touch-icon" sizes="274x274" href="/maskable_icon.png" />
 </svelte:head>
 
 <PageTransition key={url}>
--- javascript/client/src/routes/cell/index.svelte
+++ javascript/client/src/routes/cell/index.svelte
@@ -40,6 +40,10 @@
 
 <svelte:head>
 	<title>New Cell | CellWall</title>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<meta name="apple-mobile-web-app-title" content="CellWall" />
+	<link rel="apple-touch-icon" sizes="274x274" href="/maskable_icon.png" />
 </svelte:head>
 
 {#if !autoJoin}
