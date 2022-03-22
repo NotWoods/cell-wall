88afb93513fba3019dcb18ef748a07fbfd042f3e
NotWoods <NotWoods@users.noreply.github.com>
2022-03-22 00:35:58 +0000

    Add disconnect indicator

--- javascript/client/build/server/entries/pages/cell/frame/__layout.svelte.js
+++ javascript/client/build/server/entries/pages/cell/frame/__layout.svelte.js
@@ -1,7 +1,12 @@
-import { g as getContext, c as create_ssr_component, a as subscribe, s as setContext } from "../../../../chunks/index-4d214b4e.js";
+import { g as getContext, c as create_ssr_component, a as subscribe, s as setContext, b as add_attribute } from "../../../../chunks/index-4d214b4e.js";
 import { c as cellState, f as frameUrl, a as connect } from "../../../../chunks/state-socket-c1d8cd72.js";
 import "../../../../chunks/cell-state-schema-a24ecc56.js";
 import "../../../../chunks/index-23b4b723.js";
+var __layout_svelte_svelte_type_style_lang = "";
+const css = {
+  code: ".socket-error.svelte-oss7oo{position:absolute;top:0;right:0;z-index:1}",
+  map: null
+};
 function getFrameContext() {
   return getContext("frame");
 }
@@ -18,19 +23,26 @@ const load = async ({ url }) => {
 const _layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
   let $state, $$unsubscribe_state;
   let { serial } = $$props;
+  let socketState = "open";
   const socket = connect();
   const state = cellState(socket);
   $$unsubscribe_state = subscribe(state, (value) => $state = value);
   setContext("frame", { socket, state });
-  socket?.addEventListener("error", (event) => console.error("Socket error", event));
-  socket?.addEventListener("open", (event) => console.info("Socket open", event));
-  socket?.addEventListener("close", (event) => console.info("Socket close", event));
+  socket?.addEventListener("error", () => {
+    socketState = "error";
+  });
+  socket?.addEventListener("close", () => {
+    socketState = "close";
+  });
   if ($$props.serial === void 0 && $$bindings.serial && serial !== void 0)
     $$bindings.serial(serial);
+  $$result.css.add(css);
   frameUrl($state.type, serial);
   $$unsubscribe_state();
   return `${$$result.head += `${$$result.title = `<title>Cell</title>`, ""}`, ""}
 
-${slots.default ? slots.default({}) : ``}`;
+${slots.default ? slots.default({}) : ``}
+
+${socketState !== "open" ? `<svg class="${"socket-error svelte-oss7oo"}" xmlns="${"http://www.w3.org/2000/svg"}" viewBox="${"0 0 24 24"}" height="${"24px"}" width="${"24px"}"${add_attribute("fill", socketState === "error" ? "#DC2626" : "#EA580C", 0)} stroke="${"#fff"}"><path d="${"M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"}"></path></svg>` : ``}`;
 });
 export { _layout as default, getFrameContext, load };
--- javascript/client/build/server/nodes/14.js
+++ javascript/client/build/server/nodes/14.js
@@ -1,6 +1,6 @@
 import * as module from '../entries/pages/cell/frame/__layout.svelte.js';
 
 export { module };
-export const entry = 'pages/cell/frame/__layout.svelte-321b945f.js';
-export const js = ["pages/cell/frame/__layout.svelte-321b945f.js","chunks/vendor-92048905.js","chunks/state-socket-bd1aeee5.js","chunks/singletons-d1fb5791.js","chunks/cell-state-schema-a24ecc56.js"];
-export const css = [];
+export const entry = 'pages/cell/frame/__layout.svelte-c29f7196.js';
+export const js = ["pages/cell/frame/__layout.svelte-c29f7196.js","chunks/vendor-92048905.js","chunks/state-socket-bd1aeee5.js","chunks/singletons-d1fb5791.js","chunks/cell-state-schema-a24ecc56.js"];
+export const css = ["assets/pages/cell/frame/__layout.svelte-48218eda.css"];
