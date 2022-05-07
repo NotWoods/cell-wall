add152a6aaf8a443281c9e6490e544d3160c31d7
NotWoods <NotWoods@users.noreply.github.com>
2022-05-07 06:50:23 +0000

    [Deploy] Fix form action

--- javascript/client/build/server/nodes/18.js
+++ javascript/client/build/server/nodes/18.js
@@ -1,6 +1,6 @@
 import * as module from '../entries/pages/remote/image.svelte.js';
 
 export { module };
-export const entry = 'pages/remote/image.svelte-6b2234c1.js';
-export const js = ["pages/remote/image.svelte-6b2234c1.js","chunks/index-6bece970.js","chunks/DeviceOptions-0b036147.js","chunks/Form-ed973cb4.js","chunks/snackbar-host-d8a20024.js","chunks/web-75f832b0.js","chunks/index-2a0b6ee6.js","chunks/HorizontalField-10ed728a.js","chunks/Label-f27fee94.js","chunks/__layout-fd782b1f.js","chunks/TopBar-f9ff8a42.js","chunks/index-82b3cc87.js"];
+export const entry = 'pages/remote/image.svelte-ee6634b0.js';
+export const js = ["pages/remote/image.svelte-ee6634b0.js","chunks/index-6bece970.js","chunks/DeviceOptions-dd7641e8.js","chunks/Form-8928ba22.js","chunks/snackbar-host-d8a20024.js","chunks/web-75f832b0.js","chunks/index-2a0b6ee6.js","chunks/HorizontalField-10ed728a.js","chunks/Label-f27fee94.js","chunks/__layout-fd782b1f.js","chunks/TopBar-f9ff8a42.js","chunks/index-82b3cc87.js"];
 export const css = ["assets/__layout-f9525609.css","assets/TopBar-feb2efa9.css"];
